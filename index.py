b6b48768d40ab587526482f2bb8623e3c985a5c3
root <root@otoy.(none)>
2014-03-24 02:02:20 +0800

    deleted some uselss stuff

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import twitter, json
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/trends/<path:country_code>')
 def get_trending_keywords(country_code):
 
+	country_code = country_code.upper()
 	CONSUMER_KEY = 'd7tKl4qajMQmpwjIbPw'
 	CONSUMER_SECRET ='v5Utf5ewYrKdLe7VIHiNLBiPehSdG5gDoqEbPfHRB8A'
 	OAUTH_TOKEN = '126343897-sT0c0Fpt6qzrd91TiBbGlPWS3Uov9Z1yhtG6c2on'
@@ -15,12 +15,52 @@ def get_trending_keywords(country_code):
                            CONSUMER_KEY, CONSUMER_SECRET)
 
 	twitter_api = twitter.Twitter(auth=auth)
+	
 	WOE_ID = {'global':1, 
 				'PH':23424934,
 				'US':23424977
 	}
 
-	# us_trends = twitter_api.trends.place(_id=WOE_ID['PH'])
+	
+	if country_code in WOE_ID:
+		# print  "Country exists"+country_code
+		country_exists = True
+	else:
+		country_exists = False
+		# return "Sorry this feature is not available"+country_code
+
+
+
+	if country_exists:
+		trends_results = twitter_api.trends.place(_id=WOE_ID[country_code])
+				
+		julian = []
+
+		for child in trends_results:
+
+			if child['trends']:
+	    			for subchild in child['trends']:
+	        			
+	        			if subchild['name']:
+	        				julian.append(subchild['name'])
+	        				print subchild['name']
+
+		return 'Got trending keywords from Twitter  in  %s' % country_code;		
+		
+	else:
+		message =  'This feature is not available for this country'
+		result =  message
+		
+	
+	
+
+
+
+
+
+
+@app.route('/search/<path:keyword>')
+def search_keyword(keyword):
 	q = '#pba' 
 	count = 30	
 	lang = 'us' #tl
@@ -38,18 +78,18 @@ def get_trending_keywords(country_code):
 		# except KeyError, e: # No more results when next_results doesn't exist
 			# break
 
-	return 'done'
-	results =  json.dumps(us_trends, indent=1)
-	return results
-
-	country_code = country_code.rstrip('/');
-	return 'Get trending keywords in  %s' % country_code;
-
+	return 'Search for' + keyword
 
 
 @app.route('/')
 def hello_world():
 	return 'Welcome to index page!'
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
