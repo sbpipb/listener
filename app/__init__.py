4f5eee50a8504b3a1c0fa45c18a6d9db1737b2b7
julian macmang <julianmacmang@gmail.com>
2014-03-24 19:22:31 +0800

    mod on init and views

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 # from config import basedir
 
-#from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config.from_object('config') 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-from app import views, twitterapp
+from app import views, models, twitterapp
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import escape, request, Flask, url_for,  redirect, render_template, session
+from flask import flash, escape, request, Flask, url_for,  redirect, render_template, session
 from app import app
+from forms import LoginForm
 import twitter, json
 
 
@@ -8,12 +9,23 @@ def checkIfLogin():
 	if 'username' in session:
 		# return 'Logged in as %s' % escape(session['username'])
 		return True
-	else:	
+	else: 
 		return redirect(url_for('login'))
 		return False
 	# return login
 
 
+@app.route('/loginnew', methods = ['GET', 'POST'])
+def loginnew():
+		form = LoginForm()
+		if form.validate_on_submit():
+				flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+				return redirect('/index')
+		return render_template('login_new.html', 
+				title = 'Sign In',
+				form = form,
+				providers = app.config['OPENID_PROVIDERS'])
+						
 @app.route('/')
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -31,12 +43,23 @@ def login():
 	# <p><input type=text name=username>
 	# <p><input type=submit value=Login>
 	# </form>
-	# '''	
+	# ''' 
 	if 'username' in session:
 		return 'Logged in as %s' % escape(session['username'])
 	return 'You are not logged in'
 
 	return 'Login!'
+
+@app.route('/logout')
+def logout():
+		# remove the username from the session if it's there
+		session.pop('username', None)
+		return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
 
 
 @app.errorhandler(404)
